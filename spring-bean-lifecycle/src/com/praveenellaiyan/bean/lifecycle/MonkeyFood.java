ded40f59ff9d637cc03cb1ad5b780bf21ea70784
Praveen Ellaiyan <praveen.ellaiyan@gmail.com>
2019-07-07 20:36:23 +0530

    spring basic concept - lifecycle of bean

---
+++ spring-bean-lifecycle/src/com/praveenellaiyan/bean/lifecycle/MonkeyFood.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.praveenellaiyan.bean.lifecycle;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Praveen Ellaiyan
+ *
+ */
+@Component
+public class MonkeyFood implements Food {
+
+	@Override
+	public String getFoodDetails() {
+
+		return "It eats Banana!";
+
+	}
+
+}
