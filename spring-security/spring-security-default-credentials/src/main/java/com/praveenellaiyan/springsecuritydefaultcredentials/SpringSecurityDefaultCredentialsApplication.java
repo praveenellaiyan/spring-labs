48f1c8be0b6ca0b0a6ec8fd98af4f946bbf658a2
praveenellaiyan <praveen.ellaiyan@gmail.com>
2019-09-14 19:41:28 +0530

    spring security with default credentials

---
+++ spring-security/spring-security-default-credentials/src/main/java/com/praveenellaiyan/springsecuritydefaultcredentials/SpringSecurityDefaultCredentialsApplication.java
@@ -0,0 +1,13 @@
+package com.praveenellaiyan.springsecuritydefaultcredentials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityDefaultCredentialsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityDefaultCredentialsApplication.class, args);
+	}
+
+}
