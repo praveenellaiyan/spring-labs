0dc6d0218c16ea9aa83b7430a41d9fa2228b6d58
praveenellaiyan <praveen.ellaiyan@gmail.com>
2019-09-17 21:29:37 +0530

    demonstrate spring security by overriding default credentials

---
+++ spring-security/spring-security-configure-credentials-in-properties/src/main/java/com/praveenellaiyan/springsecuritydefaultcredentials/resource/WelcomeController.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.praveenellaiyan.springsecuritydefaultcredentials.resource;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author praveenellaiyan
+ *
+ */
+@RestController
+public class WelcomeController {
+
+	@RequestMapping(method = RequestMethod.GET, path = "/home")
+	public ResponseEntity<String> welcome() {
+		return ResponseEntity.ok("Welcome!");
+	}
+}
